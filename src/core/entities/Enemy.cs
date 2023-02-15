e368593005238ef534b9079639569b83b4ee4abc
Moises Sanchez <moijsanchez@gmail.com>
2023-02-15 03:31:34 -0800

    Added basic enemy with nav_agent

--- src/core/entities/Enemy.cs
+++ src/core/entities/Enemy.cs
@@ -1,42 +1,35 @@
 using Godot;
 using System;
+#pragma warning disable CS8632
 
 public partial class Enemy : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+    [Export] private int _speed = 5;
+    private PlayerController _player;
+    private NavigationAgent3D _navAgent;
+    //[Export] private NodePath _playerNodePath;
+    //[Export] private NodePath _navAgentPath;
+    
+    public override void _Ready()
+    {
+        _player = GetNode<PlayerController>("../Player");
+        _navAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
+        UpdateTargetLocation(_player!.GlobalPosition);
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
+    public override void _PhysicsProcess(double delta)
+    {
+        var currentPosition = GlobalPosition;
+        var nextPosition = _navAgent.GetNextPathPosition();
+        var updatedVelocity = (nextPosition - currentPosition).Normalized() * _speed;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y -= gravity * (float)delta;
+        Velocity = updatedVelocity;
+        MoveAndSlide();
+    }
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+    private void UpdateTargetLocation(Vector3 targetLocation)
+    {
+        _navAgent.TargetPosition = targetLocation;
+    }
 }
