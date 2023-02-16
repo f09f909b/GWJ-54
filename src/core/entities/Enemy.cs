c03193c0e625381e7f8c80dd70ff90909ee7838e
Moises Sanchez <moijsanchez@gmail.com>
2023-02-16 01:36:11 -0800

    Improved pursue + wander mechanics. Added fire

--- src/core/entities/Enemy.cs
+++ src/core/entities/Enemy.cs
@@ -1,49 +1,63 @@
-using System.Collections.Generic;
+using System;
 using Godot;
 
 public partial class Enemy : CharacterBody3D
 {
-    [Export] private int _speed = 1;
-    [Export] private int _damage = 5;
+    [Export] public string Name { get; private set; } = "Test";
+    [Export] public int Health { get; private set; } = 10;
+    [Export] public int Speed { get; private set; }  = 1;
+    [Export] public int Damage = 5;
+    private bool _isFiring;
+
     private PlayerController _player;
     private RayCast3D _sightRaycast;
     private Area3D _hearingArea;
     private NavigationAgent3D _navAgent;
+    private Timer _attackTimer;
     private State _currentState;
+
+    [Export] private NodePath _sightRangePath;
+    [Export] private NodePath _hearingAreaPath;
+    [Export] private NodePath _navigationAgent3DPath;
+    [Export] private NodePath _attackTimerPath;
 
     private enum State
     {
+        Pursue,
         Wander,
-        Chase,
-        Attack
+        Dead
     }
 
     public override void _Ready()
     {
         _player = GetNode<PlayerController>("../Player");
-        _sightRaycast = GetNode<RayCast3D>("SightRange");
-        _hearingArea = GetNode<Area3D>("HearingArea");
-        _navAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
+        _sightRaycast = GetNode<RayCast3D>(_sightRangePath);
+        _hearingArea = GetNode<Area3D>(_hearingAreaPath);
+        _navAgent = GetNode<NavigationAgent3D>(_navigationAgent3DPath);
+        _attackTimer = GetNode<Timer>(_attackTimerPath);
     }
 
     public override void _PhysicsProcess(double delta)
     {
         TrackTarget();
-        GD.Print(_currentState);
+        
         switch (_currentState)
         {
-            case State.Chase:
+            case State.Pursue:
             {
+                if (TargetHitByRayCheck("Player") && !_isFiring)
+                {
+                    EnableFire();
+                }
+
                 ChaseTarget();
-                break;
-            }
-            case State.Attack:
-            {
-                Attack();
                 break;
             }
             case State.Wander:
                 Wander();
+                break;
+            case State.Dead:
+                Die();
                 break;
             default:
                 Wander();
@@ -51,25 +65,45 @@ public partial class Enemy : CharacterBody3D
         }
     }
 
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
     private void TrackTarget()
     {
         _sightRaycast.LookAt(_player.GlobalTransform.Origin);
-        if (_sightRaycast.IsColliding())
+
+        if (TargetHitByRayCheck("Player"))
         {
-            var collidedObject = (Node) _sightRaycast.GetCollider();
-            if (collidedObject.IsInGroup("Player"))
-            {
-                OnTargetDetected();
-            }
+            _currentState = State.Pursue;
         }
         else if (TargetEnterHearingZoneCheck(_hearingArea))
         {
-            OnTargetDetected();
+            _currentState = State.Pursue;
         }
         else
         {
-            OnTargetLost();
+            _currentState = State.Wander;
         }
+    }
+
+    private bool TargetHitByRayCheck(string targetGroup)
+    {
+        if (_sightRaycast.IsColliding())
+        {
+            var collidedObject = (Node) _sightRaycast.GetCollider();
+            if (collidedObject.IsInGroup(targetGroup))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private bool TargetEnterHearingZoneCheck(Area3D area)
@@ -86,23 +120,22 @@ public partial class Enemy : CharacterBody3D
         return false;
     }
 
-    private void OnTargetDetected()
-    {
-        _currentState = State.Chase;
-    }
-
-    private void OnTargetLost()
-    {
-        _currentState = State.Wander;
-    }
-
     private void Wander()
     {
+        // TODO: Insert wandering code
     }
 
-    private void Attack()
+    private void EnableFire()
     {
-        _currentState = State.Attack;
+        _isFiring = true;
+        _attackTimer.Start();
+    }
+
+    private void Fire()
+    {
+        _player.TakeDamage(Damage);
+        _isFiring = false;
+        _attackTimer.Stop();
     }
 
     private void ChaseTarget()
@@ -110,7 +143,7 @@ public partial class Enemy : CharacterBody3D
         UpdateTargetLocation(_player.GlobalTransform.Origin);
         Vector3 currentPosition = GlobalPosition;
         Vector3 nextPosition = _navAgent.GetNextPathPosition();
-        Vector3 updatedVelocity = (nextPosition - currentPosition).Normalized() * _speed;
+        Vector3 updatedVelocity = (nextPosition - currentPosition).Normalized() * Speed;
 
         Velocity = updatedVelocity;
         MoveAndSlide();
@@ -119,5 +152,12 @@ public partial class Enemy : CharacterBody3D
     private void UpdateTargetLocation(Vector3 targetLocation)
     {
         _navAgent.TargetPosition = targetLocation;
+    }
+
+    private void Die()
+    {
+        _currentState = State.Dead;
+        // Play dead animation
+        SetProcess(false);
     }
 }
